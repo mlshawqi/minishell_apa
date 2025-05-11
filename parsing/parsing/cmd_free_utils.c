c3dea2ec19b093852579c38d0786c25d7a6ff00a
mlshawqi <marriyash77@.com>
2025-05-11 13:06:54 +0200

    linking

---
+++ parsing/parsing/cmd_free_utils.c
@@ -0,0 +1,36 @@
+#include "../minishell.h"
+
+
+void	free_command(t_cmd *cmd, void (*del)(void *))
+{
+	if (!cmd)
+		return ;
+	if (cmd->command)
+		(*del)(cmd->command);
+	if (cmd->args)
+		free_string_array(cmd->args);
+	if (cmd->pipex)
+	{
+		if(cmd->pipex->path)
+			free_str_null(&cmd->pipex->path);
+		free(cmd->pipex);
+		cmd->pipex = NULL;
+	}
+	if (cmd->io_fds)
+		free_in_out(cmd->io_fds);
+	(*del)(cmd);
+}
+
+
+void	free_command_list(t_cmd **cmd_list, void (*del)(void *))
+{
+	t_cmd	*temp;
+
+	temp = NULL;
+	while (*cmd_list)
+	{
+		temp = (*cmd_list)->next;
+		free_command(*cmd_list, del);
+		*cmd_list = temp;
+	}
+}
